08073db2d9eba8bd262b3ffca0dbe46462e1853b
ChiradeepDhar <dhar.chiradeep@gmail.com>
2024-04-09 22:45:13 +0530

    server.js

--- server/models/usermodel.ts
+++ server/models/usermodel.ts
@@ -60,3 +60,18 @@ const userSchema: Schema<IUser> =new mongoose.Schema({
 
 
 //Hash password
+userSchema.pre<IUser>('save',async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+    this.password =await bcrypt.hash(this.password,10);
+    next();
+});
+
+//compare password
+userSchema.methods.comparePassword =async function(enteredPassword:string): Promise<boolean>{
+    return await bcrypt.compare(enteredPassword ,this.password);
+};
+
+const userModel: Model<IUser> = mongoose.model("User",userSchema);
+export default userModel;
