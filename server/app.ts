0957e40fe722421418c1b15ba41dc64f1a8c910b
ChiradeepDhar <dhar.chiradeep@gmail.com>
2024-04-09 16:20:55 +0530

    usermodel.ts

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,35 @@
-import express from "express";
-export const app =express();
+
+require('dotenv').config();
+import express,{NextFunction ,Request,Response} from "express";
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import {ErrorMiddleware} from "./middleware/error";
+
+//body parser 
+app.use(express.json({limit:"50mb"}));
+
+//cookie parser
+app.use(cookieParser());
+
+//cors => cross origin resource sharing 
+app.use(cors({
+    origin:process.env.ORIGIN
+}));
+
+//TESTING API 
+
+app.get("/test",(req:Request, res:Response ,next:NextFunction) =>{
+    res.status(200).json({
+        success:"API is working",
+    });
+});
+
+//unknown route 
+app.all("*",(req:Request, res:Response,next:NextFunction) =>{
+    const err =new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+});
+
+app.use(ErrorMiddleware);
