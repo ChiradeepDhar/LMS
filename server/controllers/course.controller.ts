3bbd4f17cf20bca32c898fd1f3a15ad74cb15750
ChiradeepDhar <dhar.chiradeep@gmail.com>
2024-04-15 23:31:57 +0530

    Update CourseController.ts

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction,Request,Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from "cloudinary";
@@ -7,68 +7,68 @@ import CourseModel from "../models/course.model";
 import { redis } from "../utils/redis";
 
 //upload course
-export const uploadCourse =CatchAsyncError(async(req:Request,res:Response,next:NextFunction) =>{
-   
-    try{
-        const data =req.body;
-        const thumbnail =data.thumbnail;
-        if(thumbnail){ 
-            const mycloud =await cloudinary.v2.uploader.upload(thumbnail,{
-                folder:"courses"
+export const uploadCourse = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        const data = req.body;
+        const thumbnail = data.thumbnail;
+        if (thumbnail) {
+            const mycloud = await cloudinary.v2.uploader.upload(thumbnail, {
+                folder: "courses"
             });
 
-            data.thumbnail ={
-                public_id:mycloud.public_id,
-                url:mycloud.secure_url
+            data.thumbnail = {
+                public_id: mycloud.public_id,
+                url: mycloud.secure_url
             }
         }
 
-        
-        createCourse(data,res,next);
 
-    }catch(error:any){
-        return next(new ErrorHandler(error.message,500));
+        createCourse(data, res, next);
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
     };
 });
 
 
 //edit course
-export const editCourse =CatchAsyncError(
-    async(req:Request,res:Response,next:NextFunction ) =>{
-        try{
-            const data=req.body;
-            const thumbnail =data.thumbnail;
+export const editCourse = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const data = req.body;
+            const thumbnail = data.thumbnail;
 
-            if(thumbnail){
+            if (thumbnail) {
                 await cloudinary.v2.uploader.destroy(thumbnail.public_id);
 
-                const myCloud =await cloudinary.v2.uploader.upload(thumbnail,{
-                    folder:"Courses",
+                const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+                    folder: "Courses",
                 });
 
-                data.thumbnail={
-                    public_id:myCloud.public_id,
-                    url:myCloud.secure_url,
+                data.thumbnail = {
+                    public_id: myCloud.public_id,
+                    url: myCloud.secure_url,
                 };
             }
 
-            const courseId =req.params.id;
+            const courseId = req.params.id;
 
-            const course =await CourseModel.findByIdAndUpdate(
+            const course = await CourseModel.findByIdAndUpdate(
                 courseId,
                 {
-                    $set:data,
+                    $set: data,
                 },
-                {new:true}
+                { new: true }
             );
 
             res.status(201).json({
-                success:true,
+                success: true,
                 course,
             });
 
-        }catch(error:any){
-            return next(new ErrorHandler(error.message,500));
+        } catch (error: any) {
+            return next(new ErrorHandler(error.message, 500));
         }
     }
 );
@@ -76,67 +76,67 @@ export const editCourse =CatchAsyncError(
 
 //GET SINGLE-COURSE-WITHOUT PURCHASING
 //problem in this code
-export const getSingleCourse =CatchAsyncError(async(req:Request,res:Response,next:NextFunction) =>{
-    try{
-        const courseId =req.params.id;
+export const getSingleCourse = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const courseId = req.params.id;
 
-        const isCacheExist =await redis.get(courseId);
+        const isCacheExist = await redis.get(courseId);
         console.log("hitting redis");
 
 
-        if(isCacheExist){
-            const course =JSON.parse(isCacheExist);
+        if (isCacheExist) {
+            const course = JSON.parse(isCacheExist);
             res.status(200).json({
-                success:true,
+                success: true,
                 course,
             });
         }
-        else{
-            const course =await CourseModel.findById(req.params.id).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links");
+        else {
+            const course = await CourseModel.findById(req.params.id).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links");
 
             console.log("hitting mongodb");
 
-            await redis.set(courseId,JSON.stringify(course));
+            await redis.set(courseId, JSON.stringify(course));
 
             res.status(200).json({
-                success:true,
+                success: true,
                 course,
             });
         }
-   
-    }catch(error:any){
-        return next(new ErrorHandler(error.message,500));
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
     }
 });
 
 
 //get all courses -- without purchasing
 //problem in this code
-export const getAllCourses =CatchAsyncError(async(req:Request,res:Response,next:NextFunction) =>{
-    try{
-        const isCacheExist =await redis.get("allCourses");
-        if(isCacheExist){
-            const courses=JSON.parse(isCacheExist);
+export const getAllCourses = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const isCacheExist = await redis.get("allCourses");
+        if (isCacheExist) {
+            const courses = JSON.parse(isCacheExist);
             // console.log("hitting redis");
 
             res.status(200).json({
-                success:true,
+                success: true,
                 courses,
             });
-        }else{
-            const courses= await CourseModel.find().select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links");
+        } else {
+            const courses = await CourseModel.find().select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links");
 
             // console.log("hitting mongodb");
-            
-            await redis.set("allcourses",JSON.stringify(courses));
-            
+
+            await redis.set("allcourses", JSON.stringify(courses));
+
             res.status(200).json({
-            success:true,
-            courses,
-        });
-    }
-    }catch(error:any){
-        return next(new ErrorHandler(error.message,500));
+                success: true,
+                courses,
+            });
+        }
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
     }
 });
 
