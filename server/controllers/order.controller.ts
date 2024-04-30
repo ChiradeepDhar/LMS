9e3b6ea4c06277c4bc980ab5a5984b2b9534c0a7
ChiradeepDhar <dhar.chiradeep@gmail.com>
2024-04-30 14:32:24 +0530

    ordercontroller.ts

---
+++ server/controllers/order.controller.ts
@@ -0,0 +1,56 @@
+import { NextFunction,Request,Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+import OrderModel ,{IOrder} from "../models/orderModel";
+import userModel from "../models/usermodel";
+import CourseModel from "../models/course.model";
+import path from "path";
+import ejs from "ejs";
+import sendMail from "../utils/sendMail";
+import NotificationModel from "../models/notificationModel";
+import { newOrder } from "../services/order.service";
+
+
+//create order 
+export const createOrder =CatchAsyncError(async(req:Request,res:Response,next:NextFunction) =>{
+    try{
+        const {courseId,payment_info} =req.body as IOrder;
+        
+        const user =await userModel.findById(req.user?._id);
+
+        const courseExistInUser =user?.courses.some((course:any) => course._id.toString()===courseId);
+
+        if(courseExistInUser){
+            return next(new ErrorHandler("You have already purchased this course",400));
+        }
+
+        const course =await CourseModel.findById(courseId);
+
+        if(!course){
+            return next(new ErrorHandler("Course not found",400));
+        }
+
+        const data:any ={
+            courseId: course._id,
+            userId:user?._id,
+        };
+
+        newOrder(data,res,next);
+
+        const mailData ={
+            order:{
+                _id:course._id.slice(0,6),
+                name:course.name,
+                price:course.price,
+                data: new Date().toLocaleDateString('en-US',{year:'numeric',month:'long',day:'numeric'}),
+            }
+        }
+
+        const html=await ejs.render
+
+
+    }catch(error:any){
+        return next(new ErrorHandler(error.message,500));
+    }
+})
+ 
