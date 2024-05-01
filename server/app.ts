302e7d76dcea2bb42c8b3516b55d1b48dd1cf728
ChiradeepDhar <dhar.chiradeep@gmail.com>
2024-05-01 23:46:28 +0530

    layout.controller.ts

--- server/app.ts
+++ server/app.ts
@@ -9,7 +9,8 @@ import userRouter from "./routes/userroute";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
 import notificationRouter from "./routes/notification.route";
-import analyticsRouter from "./routes/analytics.route"
+import analyticsRouter from "./routes/analytics.route";
+import layoutRouter from "./routes/layout.route";
 
 //body parser 
 app.use(express.json({limit:"50mb"}));
@@ -24,7 +25,7 @@ app.use(cors({
 
 
 //routes
-app.use("/api/v1",userRouter,orderRouter,courseRouter,notificationRouter,analyticsRouter);
+app.use("/api/v1",userRouter,orderRouter,courseRouter,notificationRouter,analyticsRouter,layoutRouter);
 
 
 
